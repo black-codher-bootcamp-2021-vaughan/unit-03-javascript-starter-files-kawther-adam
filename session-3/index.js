6583357f1eb14a40ca7260f5cd22639cafabe3cc
Kawther <kawtheradam63@gmail.com>
2021-12-12 23:19:16 +0000

    function

--- session-3/index.js
+++ session-3/index.js
@@ -2,16 +2,21 @@
 // You can see where this file is connected to your HTML in the index.html on line 14
 
 
-let a = 5;
-const b = 10;
-let c = a + b
+/*function sayHey(){
+    return"hey"
+}
 
-console.log(c);
+function conversation(){
+    var hi = sayHey();
+    console.log("hi");
+     console.log("How are you?");
+    console.log("Goodbye");
+}
 
-a=20;
+conversation();*/
 
-console.log('after a has been updated' + c);
-
-c = a + b
-
-console.log('after a has been updated with the new value' + c);
+function futureAge(name,age) {
+    const ageInFiveYears = age + 5;
+    return name +  "will be"  +  ageInFiveYears + "in 5 years!"
+}
+console.log(futureAge("Kawther", 28));
