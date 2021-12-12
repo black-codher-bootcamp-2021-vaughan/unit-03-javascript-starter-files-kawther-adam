59ffcb772cfb5e21909feb08281f1d3bff2fb1e7
Kawther <kawtheradam63@gmail.com>
2021-12-12 23:51:18 +0000

    objects,maths operator

--- session-4/index.js
+++ session-4/index.js
@@ -1,42 +1,44 @@
 // ****************************************************TASK 1 - OBJECTS****************************************************
+let personA = {
+    name: 'Kara',
+    likes: 'Apples',
+    age: '20',
+    location:'Birmingham'
+};
+console.log(personA);
+console.log(personA.name)
 
+const PersonB = {
+    name: 'Kawther',
+    likes: 'Mangoes',
+    age:'25',
+    location:'Tyseley'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
 
 
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
 
-// const x = 6;
-// const y = 4;
+const x = 18;
+ const y = 16;
 
-// const addition = x + y;
+ const addition = x + y;
+ 
+ const multiplication = x * y;
 
-// console.log("Addition: x + y " + addition);
+ console.log("Addition: x + y " + addition);
 
-// const subtraction = x - y;
+const subtraction = x - y;
 
-// console.log("Subtraction: x - y " + subtraction);
+console.log("Subtraction: x - y " + subtraction);
 
-// const multiplication = x * y;
 
-// console.log("multiplication: x * y " + multiplication);
+ console.log("multiplication: x * y " + multiplication);
 
-// const division = x * y;
+ const division = x * y;
 
-// console.log("Division: x + y " + division);
+ console.log("Division: x + y " + division);
 
 
 
