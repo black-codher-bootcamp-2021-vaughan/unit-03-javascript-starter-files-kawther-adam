cbcfee0950ebdabf57d4cda8e9bc1c75268d57d1
Kawther <kawtheradam63@gmail.com>
2021-12-13 00:25:40 +0000

    loop, for,while

--- session-6/index.js
+++ session-6/index.js
@@ -2,7 +2,14 @@
 const randomNumber = Math.round(Math.random() * 10);
 
 // ****************************************************TASK 1****************************************************
+/*let counter =1;
+//let randomNumber = 0;
 
+while (counter <=10){
+    counter=counter +1
+    const randomNumber = Math.round(Math.random() * 10);
+    console.log(randomNumber)
+}*/
 
 
 
@@ -19,9 +26,15 @@ const randomNumber = Math.round(Math.random() * 10);
 
 // ****************************************************TASK 2****************************************************
 
+let totalNumber =1;
+//let randomNumber = 0;
 
+for (let counter = 1; counter <=10; counter = counter + 1){
+    totalNumber = totalNumber + 1;
+    const randomNumber = Math.round(Math.random() * 10);
+    console.log(totalNumber)
 
-
+}
 
 
 
